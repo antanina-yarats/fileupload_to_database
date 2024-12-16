669f49fa21c5ea8555b4adf9a799afc5fbf0123b
Antanina <tonia@Antanina-MacBook-Pro.local>
2024-12-16 10:14:18 +0200

    Added tmp test to app.js for render deployment

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-
 import { router } from "./routes/routes.js";
 
 const app = new Application();
 
+// Test write access to /tmp
+try {
+  const testFile = "/tmp/test-file";
+  await Deno.writeTextFile(testFile, "This is a test.");
+  console.log("Write access to /tmp confirmed!");
+  await Deno.remove(testFile);
+} catch (err) {
+  console.error("Error writing to /tmp:", err);
+}
+
 app.use(errorMiddleware);
 app.use(renderMiddleware);
-
 app.use(router.routes());
 
 console.log("Listening on http://localhost:7777");
