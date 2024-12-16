0b5d37b564b2afeefbbb8f50e359c12ea41c8cf0
Antanina <tonia@Antanina-MacBook-Pro.local>
2024-12-16 10:57:17 +0200

    Added api to fetch latest id to avoid delay

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,16 @@ import * as formController from "./controllers/formController.js";
 
 const router = new Router();
 
-router.get("/", formController.viewForm);
+router.get("/last-id", async ({ response }) => {
+    try {
+      const lastId = await lastUploadedId(); 
+      response.body = { last_id: lastId }; 
+    } catch (err) {
+      console.error("Error fetching last uploaded ID:", err);
+      response.status = 500;
+      response.body = { error: "Internal server error." };
+    }
+  });
 router.post("/upload", formController.uploadFile);
 router.post("/files", formController.downloadFile);
 
